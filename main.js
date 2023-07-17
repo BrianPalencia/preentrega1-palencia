efe0cb8de089d56ea3bab5dedd1d1f5fd35cf4b0
Brian Palencia <briandonaldo@gmail.com>
2023-07-17 14:10:20 -0600

    arreglando preentrega

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
+const MARCA = "Toyota"
+const MODELO = "Yaris"
+const PRECIO = 60000
+const TASA = {
+    DOCE: "doce",
+    QUINCE: "quince",
+}
+const MESES = 24
+const MES_ACTUAL = 4
+
 class ComprarVehiculo{
-    constructor(marca, modelo, precio, tasa, meses){
+    constructor(marca, modelo, precio, tasa, meses, actual){
         this.marca = marca;
         this.modelo = modelo;
         this.precio = precio;
         this.tasa = tasa;
         this.meses = meses;
+        this.actual = actual
     }
     obtenerLinea(){
         return this.marca + " " + this.modelo + " - Q. " + this.precio;
     }
     calcularTasa(){
-        return (this.tasa / 100) * this.precio;
+        if (this.tasa == TASA.DOCE) {
+            return 0.12 * this.precio;
+        } else {
+            return 0.15 * this.precio
+        }
     }
     calcularTotal(){
         return this.calcularTasa() + this.precio;
@@ -18,20 +33,20 @@ class ComprarVehiculo{
     calcularCuotas(){
         return this.precio / this.meses;
     }
+    calcularRestante(){
+        let restante = 0;
+        for (let i = this.actual; i <= this.meses; i++) {
+            restante += this.calcularCuotas()
+        }
+
+        return restante
+    }
 }
 
-const formulario= document.getElementById("formulario");
-
-formulario.addEventListener("submit", function(e){
-    e.preventDefault();
-    const marca = document.getElementById("marca").value;
-    const modelo = document.getElementById("modelo").value;
-    const precio = document.getElementById("precio").value;
-    const tasa = document.getElementById("tasa").value;
-    const meses = document.getElementById("meses").value;
-    const vehiculo = new ComprarVehiculo(marca, modelo, Number(precio), Number(tasa), Number(meses));
-    const linea = vehiculo.obtenerLinea();
-    const total = vehiculo.calcularTotal();
-    const cuotas = vehiculo.calcularCuotas();
-    document.getElementById("resultado").innerHTML = "Vehiculo: " + linea + "<br />Total a pagar: Q. " + total + "<br />Cuotas de: Q. " + cuotas + " por " + meses + " meses.";
-});
+const vehiculo = new ComprarVehiculo(MARCA, MODELO, PRECIO, TASA.QUINCE, MESES, MES_ACTUAL);
+const linea = vehiculo.obtenerLinea();
+const total = vehiculo.calcularTotal();
+const cuotas = vehiculo.calcularCuotas();
+const restante = vehiculo.calcularRestante();
+console.log("Vehiculo: " + linea + "\nTotal a pagar: Q. " + total + "\nCuotas de: Q. " + cuotas + " por " + MESES + " meses.")
+console.log("Despues de " + MES_ACTUAL + " meses usted debe un total de: Q. " + restante)
